16866424d5bf6ebec3a3e37a276e9f28818aab43
Edward Dai <edward.y.dai@gmail.com>
2025-11-23 16:34:13 -0800

    Added End Reset

--- LivingRoomStart.gd
+++ LivingRoomStart.gd
@@ -3,10 +3,11 @@ extends Node3D
 @export var GameScene: PackedScene
 @export var CamTarget: PhantomCamera3D
 @export var time_in_seconds : int
+@onready var TV : AudioStreamPlayer = $AudioStreamPlayer
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	TV.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
